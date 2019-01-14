f29dab12a36b05c8ca80c897b7c850589d82f9e7
bincyalias1994 <44706136+bincyalias1994@users.noreply.github.com>
2019-01-14 16:56:53 -0500

    WEEK 2 CLASS HAND IN WORKS

---
+++ Script1.js
@@ -0,0 +1,14 @@
+//Name: Bincy Alias
+//Date: January 14,2019
+//Student: C0745111
+//course: CSD 2214 Section 6
+
+let countDown;
+let timeInterval;
+const endDate = document.querySelector('input[name="endDate"]');
+console.log(endDate.value);
+const clock = document.querySelector('clock');
+const daysSpan = document.querySelector('.days');
+const hoursSpan = document.querySelector('.hours');
+const minutesSpan = document.querySelector('.minutes');
+const secondsSpan = document.querySelector('.seconds');
